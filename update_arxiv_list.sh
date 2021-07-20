27961ffd7cb2b24a25fc73ad39d3893713261b25
laura-clinton <laura@phasecraft.io>
2021-07-20 12:50:36 +0100

    updated path variable etc

--- update_arxiv_list.sh
+++ update_arxiv_list.sh
@@ -1,10 +1,9 @@
-source activate py35
 export PYTHONIOENCODING=utf8
 
 git fetch --all
 git reset --hard origin/master
 
-//anaconda/envs/py35/bin/python create_group_arxiv_html.py 
+//usr/bin/python3 create_group_arxiv_html.py 
 
 git add .
 git commit -m "synced @ $(date)"
